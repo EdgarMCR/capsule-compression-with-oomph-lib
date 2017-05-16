31bfc395c720aefb989fc09f476d22f7d1e8852a
Edgar Haener <edgar.haner@gmail.com>
2017-05-16 17:50:03 +0100

    Outputing force for soft contact

--- axisym_solid_traction_elements.h
+++ axisym_solid_traction_elements.h
@@ -2351,6 +2351,9 @@ public:
    //Do I nned to add in derivativs w.r.t. to external data? Not in original...
   }
 
+ //Integrate pressure over the element
+ void  resulting_contact_force( Vector<double> &contact_force);
+
  // Returns pressure and penetration at each integration point
  // The vectors that are provided should be empty
  void get_pressure_and_penetration(Vector<double> &x_cart,
@@ -2487,6 +2490,11 @@ public:
 
      double pressure = get_contact_pressure(x, xi);
      outfile << pressure << " ";
+
+     Vector<double> force(2);
+     this->resulting_contact_force(force);
+     outfile <<  force[0] << " "; 
+     outfile <<  force[1] << " "; 
      
      outfile << std::endl;
 
@@ -2769,6 +2777,202 @@ fill_in_contribution_to_residuals(Vector<double> &residuals)
     } //End of loop over shape functions
   } //End of loop over integration points
 }
+
+//=====================================================================
+/// Resulting contact force
+//=====================================================================
+template<class ELEMENT>
+void AxisymmetricSolidTractionSoftContactElement<ELEMENT>::resulting_contact_force(
+ Vector<double> &contact_force) 
+{
+ //Find out how many nodes there are
+ unsigned n_node = this->nnode();
+   
+ //Find out how many positional dofs there are
+ unsigned n_position_type = this->nnodal_position_type();
+   
+ //Find out the dimension of the node
+ unsigned n_dim = this->nodal_dimension();
+
+ //std::cout << "About to initialise vector." << std::endl;
+
+ // Initialise
+ for (unsigned i=0;i<n_dim;i++)
+  {
+   contact_force[i]=0.0;
+  }
+
+ //Set up memory for the shape functions
+ //Note that in this case, the number of lagrangian coordinates is always
+ //equal to the dimension of the nodes
+ Shape psi(n_node,n_position_type);
+ DShape dpsids(n_node,n_position_type,n_dim-1); 
+   
+ // Separate shape functions for Lagrange multiplier
+ Shape psi_p(n_node);
+ Vector<double> s(n_dim-1);
+   
+ //Set the value of n_intpt
+ unsigned n_intpt = this->integral_pt()->nweight();
+   
+ // std::cout << "Starting loop over integration points." << std::endl;
+ //Loop over the integration points
+ for(unsigned ipt=0;ipt<n_intpt;ipt++)
+  {
+   //Get the integral weight
+   double w = this->integral_pt()->weight(ipt);
+     
+   //Only need to call the local derivatives
+   this->dshape_local_at_knot(ipt,psi,dpsids);
+     
+      
+   //Calculate the Eulerian and Lagrangian coordinates 
+   Vector<double> interpolated_x(n_dim,0.0);
+
+//Added by Edgar, inefficient duplication for the time being so I don't have 
+//to think
+//TODO: fix
+
+   Vector<double> interpolated_xi(2,0.0);
+
+   //Calculate the global and lagrangian derivtives wrt the local coordinates
+   Vector<double> interpolated_dxds(2,0.0); 
+   Vector<double> interpolated_dxids(2,0.0);
+     
+   //Also calculate the surface Vectors (derivatives wrt local coordinates)
+   DenseMatrix<double> interpolated_A(n_dim-1,n_dim,0.0);   
+     
+   //Calculate displacements and derivatives
+   for(unsigned l=0;l<n_node;l++) 
+    {       
+     //Loop over positional dofs
+     for(unsigned k=0;k<n_position_type;k++)
+      {
+       //Loop over displacement components (deformed position)
+       for(unsigned i=0;i<n_dim;i++)
+        {
+         //Calculate the Eulerian and Lagrangian positions
+         interpolated_x[i] += 
+          this->nodal_position_gen(l,this->bulk_position_type(k),i)*psi(l,k);
+           
+        interpolated_xi[i] += 
+          this->lagrangian_position_gen(l,this->bulk_position_type(k),i)*psi(l,k);
+
+         //Calculate the derivatives of the global and lagrangian coordinates
+         interpolated_dxds[i] += 
+          this->nodal_position_gen(l,this->bulk_position_type(k),i)*dpsids(l,k,0);
+
+         interpolated_dxids[i] += 
+          this->lagrangian_position_gen(l,this->bulk_position_type(k),i)
+          *dpsids(l,k,0);
+
+         //Loop over LOCAL derivative directions, to calculate the tangent(s)
+         for(unsigned j=0;j<n_dim-1;j++)
+          {
+           interpolated_A(j,i) += 
+            this->nodal_position_gen(l,this->bulk_position_type(k),i)*dpsids(l,k,j);
+          }
+        }
+      }
+    }
+   //   std::cout << "Creating A now " << std::endl;
+     
+ //Now find the local deformed metric tensor from the tangent Vectors
+   DenseMatrix<double> A(2);
+   //The off-diagonal terms are Zero 
+   A(0,1) = A(1,0) = 0.0;
+   //The diagonal terms are a little complicated
+   A(0,0) =  
+    (interpolated_dxds[0] - interpolated_x[1]*interpolated_dxids[1])*
+    (interpolated_dxds[0] - interpolated_x[1]*interpolated_dxids[1]) +
+    (interpolated_dxds[1] + interpolated_x[0]*interpolated_dxids[1])*
+    (interpolated_dxds[1] + interpolated_x[0]*interpolated_dxids[1]);
+
+
+   A(1,1) =  (interpolated_x[0]*sin(interpolated_xi[1]) +
+               interpolated_x[1]*cos(interpolated_xi[1]))*
+    (interpolated_x[0]*sin(interpolated_xi[1]) +
+     interpolated_x[1]*cos(interpolated_xi[1]));
+
+   //Premultiply the weights and the square-root of the determinant of 
+   //the metric tensor
+   double W = w*sqrt(A(0,0)*A(1,1));
+   
+   //Get the outer unit normal
+   Vector<double> interpolated_normal(2);
+   
+   /*if(b)
+   {
+        std::cout << "ipt = " << ipt << std::endl;
+   }*/
+   //New method of finding outer unit normal is to call function
+   //this->outer_unit_normal(ipt,interpolated_normal);
+   
+   //Old way
+
+   //Also find the normal -- just the cross product of the metric tensors
+   //but I want to express it in terms of e_r and e_theta components
+   //N.B. There is an issue at theta = 0,pi, where the normal is e_{r},
+   //but given that I never assemble it, should be OK!
+   //The minus sign is chosen to ensure that the normal is really outward 
+
+   //Component in the e_{r} direction
+   interpolated_normal[0] = -1.0*
+    (interpolated_x[0]*sin(interpolated_xi[1]) +
+     interpolated_x[1]*cos(interpolated_xi[1]))*
+    (interpolated_dxds[1] + interpolated_x[0]*interpolated_dxids[1]);
+   //Component in the e_{theta} direction
+   interpolated_normal[1] =  -1.0*
+    (interpolated_x[0]*sin(interpolated_xi[1]) +
+     interpolated_x[1]*cos(interpolated_xi[1]))*
+    (interpolated_x[1]*interpolated_dxids[1] - interpolated_dxds[0]);
+   
+   //TODO: Fix normal direction!
+   /* Huge assumption: we are not going to be on north or south face
+   //If we're on the north or south face need to flip normal
+   if(s_fixed_value()==-1)
+    {
+     interpolated_normal[0] *= -1.0;
+     interpolated_normal[1] *= -1.0;
+    }
+    */
+	
+   //Now adjust and scale the normal
+   double length = 0.0;
+   for(unsigned i=0;i<2;i++)
+    {
+     interpolated_normal[i] *= this->normal_sign();
+     length += interpolated_normal[i]*interpolated_normal[i];
+    }
+   for(unsigned i=0;i<2;i++)
+    {
+     interpolated_normal[i] /= sqrt(length);
+    }
+
+
+   // Now calculate contact pressure
+   double pressure = get_contact_pressure(interpolated_x, interpolated_xi);
+     
+   // Calculate the "load" -- Lagrange multiplier acts as traction to
+   // to enforce required surface displacement
+   Vector<double> traction(n_dim);
+   for (unsigned i=0;i<n_dim;i++)
+    {
+     traction[i]=-pressure*interpolated_normal[i];
+    }
+     
+   // Add to resulting force
+   for (unsigned i=0;i<n_dim;i++)
+    {
+     contact_force[i]+=traction[i]*W;
+     
+    }
+    
+   
+  }
+ 
+}
+
 
 } // End of namespace
 #endif
